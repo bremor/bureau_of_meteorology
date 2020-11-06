82c5d25d53ef2a98bddf37f5b1d40522e0886fb2
bremor <34525505+bremor@users.noreply.github.com>
2020-11-06 11:07:43 +1100

    Update collector.py

--- custom_components/bureau_of_meteorology/PyBoM/collector.py
+++ custom_components/bureau_of_meteorology/PyBoM/collector.py
@@ -11,6 +11,34 @@ _LOGGER = logging.getLogger(__name__)
 MIN_TIME_BETWEEN_UPDATES = datetime.timedelta(minutes=10)
 DAILY_FORECASTS_URL = "https://api.weather.bom.gov.au/v1/locations/{}/forecasts/daily"
 LOCATIONS_URL = "https://api.weather.bom.gov.au/v1/locations/{}"
+MDI_ICON_MAP = {
+    'clear': 'mdi:weather-night',
+    'cloudy': 'mdi:weather-cloudy',
+    'cyclone': 'mdi:weather-hurricane',
+    'dust': 'mdi:weather-hazy',
+    'dusty': 'mdi:weather-hazy',
+    'fog': 'mdi:weather-fog',
+    'frost': 'mdi:snowflake-melt',
+    'haze': 'mdi:weather-hazy',
+    'hazy': 'mdi:weather-hazy',
+    'heavy_shower': 'mdi:weather-pouring',
+    'heavy_showers': 'mdi:weather-pouring',
+    'light_rain': 'mdi:weather-partly-rainy',
+    'light_shower': 'mdi:weather-light-showers',
+    'light_showers': 'mdi:weather-light-showers',
+    "mostly_sunny": "mdi:weather-sunny",
+    'partly_cloudy': 'mdi:weather-partly-cloudy',
+    'rain': 'mdi:weather-pouring',
+    'shower': 'mdi:weather-rainy',
+    'showers': 'mdi:weather-rainy',
+    'snow': 'mdi:weather-snowy',
+    'storm': 'mdi:weather-lightning-rainy',
+    'storms': 'mdi:weather-lightning-rainy',
+    'sunny': 'mdi:weather-sunny',
+    'tropical_cyclone': 'mdi:weather-hurricane',
+    'wind': 'mdi:weather-windy',
+    'windy': 'mdi:weather-windy',
+}
 OBSERVATIONS_URL = "https://api.weather.bom.gov.au/v1/locations/{}/observations"
 
 
@@ -34,6 +62,7 @@ class Collector:
         if response is not None and response.status == 200:
             locations_data = await response.json()
             self.location_name = locations_data["data"]["name"]
+            return True
 
     async def get_observations_data(self):
         """Get JSON observations data from BOM API endpoint."""
@@ -45,19 +74,20 @@ class Collector:
         if response is not None and response.status == 200:
             self.observations_data = await response.json()
             await self.flatten_observations_data()
-            return True
 
     async def flatten_observations_data(self):
         """Flatten out wind and gust data."""
         flattened = {}
 
-        flattened["wind_speed_kilometre"] = self.observations_data["data"]["wind"]["speed_kilometre"]
-        flattened["wind_speed_knot"] = self.observations_data["data"]["wind"]["speed_knot"]
-        flattened["wind_direction"] = self.observations_data["data"]["wind"]["direction"]
+        wind = self.observations_data["data"]["wind"]
+        flattened["wind_speed_kilometre"] = wind["speed_kilometre"]
+        flattened["wind_speed_knot"] = wind["speed_knot"]
+        flattened["wind_direction"] = wind["direction"]
 
         if self.observations_data["data"]["gust"] is not None:
-            flattened["gust_speed_kilometre"] = self.observations_data["data"]["gust"]["speed_kilometre"]
-            flattened["gust_speed_knot"] = self.observations_data["data"]["gust"]["speed_knot"]
+            gust = self.observations_data["data"]["gust"]
+            flattened["gust_speed_kilometre"] = gust["speed_kilometre"]
+            flattened["gust_speed_knot"] = gust["speed_knot"]
         else:
             flattened["gust_speed_kilometre"] = None
             flattened["gust_speed_knot"] = None
@@ -80,15 +110,22 @@ class Collector:
         """Flatten out forecast data."""
         flattened = {}
         for day in range(0, 6):
-            flattened["uv_category"] = self.daily_forecasts_data["data"][day]["uv"]["category"]
-            flattened["uv_max_index"] = self.daily_forecasts_data["data"][day]["uv"]["max_index"]
-            flattened["uv_start_time"] = self.daily_forecasts_data["data"][day]["uv"]["start_time"]
-            flattened["uv_end_time"] = self.daily_forecasts_data["data"][day]["uv"]["end_time"]
-            flattened["rain_amount_min"] = self.daily_forecasts_data["data"][day]["rain"]["amount"]["min"]
-            flattened["rain_amount_max"] = self.daily_forecasts_data["data"][day]["rain"]["amount"]["max"]
-            flattened["rain_chance"] = self.daily_forecasts_data["data"][day]["rain"]["chance"]
+            icon = self.daily_forecasts_data["data"][day]["icon_descriptor"]
+            flattened["mdi_icon"] = MDI_ICON_MAP[icon]
+
+            uv = self.daily_forecasts_data["data"][day]["uv"]
+            flattened["uv_category"] = uv["category"]
+            flattened["uv_max_index"] = uv["max_index"]
+            flattened["uv_start_time"] = uv["start_time"]
+            flattened["uv_end_time"] = uv["end_time"]
+
+            rain = self.daily_forecasts_data["data"][day]["rain"]
+            flattened["rain_amount_min"] = rain["amount"]["min"]
+            flattened["rain_amount_max"] = rain["amount"]["max"]
+            flattened["rain_chance"] = rain["chance"]
+
             self.daily_forecasts_data["data"][day].update(flattened)
-        
+
     @Throttle(MIN_TIME_BETWEEN_UPDATES)
     async def async_update(self):
         """Refresh the data on the collector object."""
