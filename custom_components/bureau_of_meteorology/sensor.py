f715c643f6e4dde07031d465969ff543920a3e73
bremor <34525505+bremor@users.noreply.github.com>
2020-11-06 11:08:27 +1100

    Update sensor.py

--- custom_components/bureau_of_meteorology/sensor.py
+++ custom_components/bureau_of_meteorology/sensor.py
@@ -6,8 +6,8 @@ from homeassistant.const import (
     DEVICE_CLASS_HUMIDITY,
     DEVICE_CLASS_TEMPERATURE,
     DEVICE_CLASS_TIMESTAMP,
-    #LENGTH_MILLIMETERS,
-    #PERCENTAGE,
+    LENGTH_MILLIMETERS,
+    PERCENTAGE,
     TEMP_CELSIUS,
 )
 from homeassistant.helpers.entity import Entity
@@ -15,34 +15,33 @@ from .const import DOMAIN
 
 _LOGGER = logging.getLogger(__name__)
 
-OBSERVATIONS = {
-    "temp": ["Temperature", TEMP_CELSIUS, DEVICE_CLASS_TEMPERATURE],
-    "temp_feels_like": ["Temperature Feels Like", TEMP_CELSIUS, DEVICE_CLASS_TEMPERATURE],
-    "rain_since_9am": ["Rain Since 9am", "mm", None],
-    "humidity": ["Humidity", "%", DEVICE_CLASS_HUMIDITY],
-    "wind_speed_kilometre": ["Wind Speed", "km/h", None],
-    "wind_speed_knot": ["Wind Speed Knots", "kts", None],
-    "wind_direction": ["Wind Direction", None, None],
-    "gust_speed_kilometre": ["Gust Speed", "km/h", None],
-    "gust_speed_knot": ["Gust Speed Knots", "kts", None],
+SENSOR_NAMES = {
+    "temp": [TEMP_CELSIUS, DEVICE_CLASS_TEMPERATURE],
+    "temp_feels_like": [TEMP_CELSIUS, DEVICE_CLASS_TEMPERATURE],
+    "rain_since_9am": [LENGTH_MILLIMETERS, None],
+    "humidity": [PERCENTAGE, DEVICE_CLASS_HUMIDITY],
+    "wind_speed_kilometre": ["km/h", None],
+    "wind_speed_knot": ["kts", None],
+    "wind_direction": [None, None],
+    "gust_speed_kilometre": ["km/h", None],
+    "gust_speed_knot": ["kts", None],
+    "temp_max": [TEMP_CELSIUS, DEVICE_CLASS_TEMPERATURE],
+    "temp_min": [TEMP_CELSIUS, DEVICE_CLASS_TEMPERATURE],
+    "extended_text": [None, None],
+    "icon_descriptor": [None, None],
+    "mdi_icon": [None, None],
+    "short_text": [None, None],
+    "uv_category": [None, None],
+    "uv_max_index": ["UV", None],
+    "uv_start_time": ["UV", DEVICE_CLASS_TIMESTAMP],
+    "uv_end_time": ["UV", DEVICE_CLASS_TIMESTAMP],
+    "rain_amount_min": [LENGTH_MILLIMETERS, None],
+    "rain_amount_max": [LENGTH_MILLIMETERS, None],
+    "rain_amount_range": [LENGTH_MILLIMETERS, None],
+    "rain_chance": [PERCENTAGE, None],
+    "fire_danger": [None, None],
 }
 
-DAILY_FORECAST = {
-    "temp_max": ["Max", TEMP_CELSIUS, DEVICE_CLASS_TEMPERATURE],
-    "temp_min": ["Min", TEMP_CELSIUS, DEVICE_CLASS_TEMPERATURE],
-    "extended_text": ["Extended Text", None, None],
-    "icon_descriptor": ["Icon", None, None],
-    "short_text": ["Short Text", None, None],
-    "uv_category": ["UV Category", None, None],
-    "uv_max_index": ["UV Max Index", "UV", None],
-    "uv_start_time": ["UV Start Time", "UV", DEVICE_CLASS_TIMESTAMP],
-    "uv_end_time": ["UV End Time", "UV", DEVICE_CLASS_TIMESTAMP],
-    "rain_amount_min": ["Rain Amount Min", "mm", None],
-    "rain_amount_max": ["Rain Amount Max", "mm", None],
-    "rain_amount_range": ["Rain Amount Range", "mm", None],
-    "rain_chance": ["Rain Chance", "%", None],
-    "fire_danger": ["Fire Danger", None, None],
-}
 
 async def async_setup_entry(hass, config_entry, async_add_devices):
     """Add sensors for passed config_entry in HA."""
@@ -57,12 +56,12 @@ async def async_setup_entry(hass, config_entry, async_add_devices):
     new_devices = []
 
     for observation in collector.observations_data["data"]:
-        if observation in OBSERVATIONS:
+        if observation in SENSOR_NAMES:
             new_devices.append(ObservationSensor(collector, station_name, observation))
 
     for day in range(0,6):
         for forecast in collector.daily_forecasts_data["data"][day]:
-            if forecast in DAILY_FORECAST:
+            if forecast in SENSOR_NAMES:
                 new_devices.append(ForecastSensor(collector, collector.location_name, day, forecast))
         new_devices.append(ForecastSensor(collector, collector.location_name, day, "rain_amount_range"))
 
@@ -70,19 +69,40 @@ async def async_setup_entry(hass, config_entry, async_add_devices):
         async_add_devices(new_devices)
 
 
-class ObservationSensor(Entity):
-    """Base representation of a BOM Observation Sensor."""
+class SensorBase(Entity):
+    """Base representation of a BOM Sensor."""
 
-    def __init__(self, collector, station_name, observation):
+    def __init__(self, collector, location_name, sensor_name):
         """Initialize the sensor."""
         self.collector = collector
-        self.station_name = station_name
-        self.observation = observation
+        self.location_name = location_name
+        self.sensor_name = sensor_name
+
+    @property
+    def device_class(self):
+        """Return the name of the sensor."""
+        return SENSOR_NAMES[self.sensor_name][1]
+
+    @property
+    def unit_of_measurement(self):
+        """Return the unit of measurement."""
+        return SENSOR_NAMES[self.sensor_name][0]
+
+    async def async_update(self):
+        """Refresh the data on the collector object."""
+        await self.collector.async_update()
+
+class ObservationSensor(SensorBase):
+    """Representation of a BOM Observation Sensor."""
+
+    def __init__(self, collector, location_name, sensor_name):
+        """Initialize the sensor."""
+        super().__init__(collector, location_name, sensor_name)
 
     @property
     def unique_id(self):
         """Return Unique ID string."""
-        return f"{self.station_name}_{self.observation}"
+        return f"{self.location_name}_{self.sensor_name}"
 
     @property
     def device_state_attributes(self):
@@ -95,42 +115,26 @@ class ObservationSensor(Entity):
     @property
     def state(self):
         """Return the state of the sensor."""
-        return self.collector.observations_data["data"][self.observation]
-
-    @property
-    def unit_of_measurement(self):
-        """Return the unit of measurement."""
-        return OBSERVATIONS[self.observation][1]
+        return self.collector.observations_data["data"][self.sensor_name]
 
     @property
     def name(self):
         """Return the name of the sensor."""
-        return f"{self.station_name} {OBSERVATIONS[self.observation][0]}"
-
-    @property
-    def device_class(self):
-        """Return the name of the sensor."""
-        return OBSERVATIONS[self.observation][2]
-
-    async def async_update(self):
-        """Refresh the data on the collector object."""
-        await self.collector.async_update()
+        return f"{self.location_name} {self.sensor_name.replace('_', ' ').title()}"
 
 
-class ForecastSensor(Entity):
-    """Base representation of a BOM Forecast Sensor."""
+class ForecastSensor(SensorBase):
+    """Representation of a BOM Forecast Sensor."""
 
-    def __init__(self, collector, location_name, day, forecast):
+    def __init__(self, collector, location_name, day, sensor_name):
         """Initialize the sensor."""
-        self.collector = collector
-        self.location_name = location_name
         self.day = day
-        self.forecast = forecast
+        super().__init__(collector, location_name, sensor_name)
 
     @property
     def unique_id(self):
         """Return Unique ID string."""
-        return f"{self.location_name}_{self.day}_{self.forecast}"
+        return f"{self.location_name}_{self.day}_{self.sensor_name}"
 
     @property
     def device_state_attributes(self):
@@ -142,7 +146,7 @@ class ForecastSensor(Entity):
     @property
     def state(self):
         """Return the state of the sensor."""
-        if self.forecast == "rain_amount_range":
+        if self.sensor_name == "rain_amount_range":
             if self.collector.daily_forecasts_data["data"][self.day]["rain_amount_max"] is not None:
                 state = "{} to {}".format(
                     self.collector.daily_forecasts_data["data"][self.day]["rain_amount_min"],
@@ -151,25 +155,11 @@ class ForecastSensor(Entity):
             else:
                 state = self.collector.daily_forecasts_data["data"][self.day]["rain_amount_min"]
         else:
-            state = self.collector.daily_forecasts_data["data"][self.day][self.forecast]
+            state = self.collector.daily_forecasts_data["data"][self.day][self.sensor_name]
         return (state[:251] + '...') if type(state) == str and len(
                 state) > 251 else state
 
     @property
-    def unit_of_measurement(self):
-        """Return the unit of measurement of the sensor."""
-        return DAILY_FORECAST[self.forecast][1]
-
-    @property
     def name(self):
         """Return the name of the sensor."""
-        return f"{self.location_name} {DAILY_FORECAST[self.forecast][0]} {self.day}"
-
-    @property
-    def device_class(self):
-        """Return the device class of the sensor."""
-        return DAILY_FORECAST[self.forecast][2]
-
-    async def async_update(self):
-        """Refresh the data on the collector object."""
-        await self.collector.async_update()
+        return f"{self.location_name} {self.sensor_name.replace('_', ' ').title()} {self.day}"
