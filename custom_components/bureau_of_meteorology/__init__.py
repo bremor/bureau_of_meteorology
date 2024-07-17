50b47f71e83f7c1dbd19d27ee94919e17e8fcab5
Joshua Leaper <poshernater@outlook.com>
2024-07-17 20:15:31 +0930

    Fix setup block warning
    Fixes #228

--- custom_components/bureau_of_meteorology/__init__.py
+++ custom_components/bureau_of_meteorology/__init__.py
@@ -81,7 +81,7 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     }
 
     for component in PLATFORMS:
-        hass.async_create_task(
+        await hass.async_create_task(
             hass.config_entries.async_forward_entry_setup(entry, component)
         )
 
